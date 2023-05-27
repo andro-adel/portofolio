f5161838362a10ad8103479b70fcf5625c784052
Andro <70522108+andro-adel@users.noreply.github.com>
2023-05-27 22:00:29 +0300

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Andro Adel</span> I am looking for a challenging job with a rapidly growing organization that can provide me with a range of goals and job objectives within a contemporary and economical business setting."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
