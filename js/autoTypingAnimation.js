6fa3671b83354f27fdc5141a5e93e1b3a5b190cb
Andro Adel <andrew@dd.net.sa>
2024-02-24 17:08:01 +0200

    edit in repo

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,9 +1,13 @@
-setTimeout(function() {
-    $('.about-info-inner').typed({
-      strings: [
-        "<span>Hi, I am Andro Adel</span> I am looking for a challenging job with a rapidly growing organization that can provide me with a range of goals and job objectives within a contemporary and economical business setting."
-      ],
-      typeSpeed: 7,
-      contentType: 'html'
-    });
-  }, 100);
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      `My goal to contribute to the success of a forward-thinking organization by designing, developing, and deploying
+cutting-edge web applications. My goal is to utilize my proficiency in both front-end and back-end technologies
+to create seamless and efficient solutions, fostering a positive user experience. I am committed to staying
+abreast of emerging technologies, collaborating with cross-functional teams, and continuously improving my
+skills to deliver high-quality software solutions that meet and exceed client expectations.`,
+    ],
+    typeSpeed: 7,
+    contentType: "html",
+  });
+}, 100);
